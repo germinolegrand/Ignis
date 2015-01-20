8f1838dc27da964a24ee75dd020eaf8c897ff372
Tassim <n.tabbaza54@gmail.com>
2015-01-20 13:44:44 +0100

    Debut log::toString

---
+++ log_system/ToString.h
@@ -0,0 +1,76 @@
+#pragma once 
+
+#include <string>
+#include <type_traits>
+#include <sstream>
+
+namespace ign {
+namespace log {
+
+////////////////////////////////////////////
+// SFINAE test for toString
+////////////////////////////////////////////
+template <typename T>
+class has_toString
+{
+	typedef char one;
+	typedef long two;
+
+	template <typename C> static one test( decltype(&C::toString) ) ;
+	template <typename C> static two test(...);
+
+public:
+	enum { value = sizeof(test<T>(0)) == sizeof(char) };
+};
+
+////////////////////////////////////////////
+// ToString
+////////////////////////////////////////////
+
+/** has_toString **/
+template<class T, typename std::enable_if<has_toString<T>::value>::type* = nullptr>
+std::string toString(const T& t)
+{
+	return t.toString();
+}
+
+/** arithmetic **/
+template<class T, std::enable_if_t<std::is_arithmetic<T>::value>* = nullptr>
+std::string toString(const T& t)
+{
+	return std::to_string(t);
+}	
+
+/** pointer **/
+template<class T, std::enable_if_t<std::is_pointer<T>::value>* = nullptr>
+std::string toString(const T& t)
+{
+	std::stringstream ss;
+	ss << t;
+	return ss.str();
+}	
+
+/** array with [] **/
+template<class T, std::enable_if_t<std::is_array<T>::value>* = nullptr>
+std::string toString(const T& t)
+{
+	std::string res = "[";
+
+	for (const auto& elem : t)
+		res += toString(elem) + ", ";
+	res.pop_back();
+	res.pop_back();
+	res += "]";	
+
+	return res; 
+}	
+
+/** Other **/
+template<class T, std::enable_if_t<std::is_class<T>::value && !has_toString<T>::value>* = nullptr>
+std::string toString(const T& t)
+{
+	return std::string("Class");
+}	
+
+}
+}
